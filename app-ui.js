eabf3bb88324da91ce18c5a64b8b0478322adc65
Michael Stock <mistock@outlook.com>
2026-08-14 10:50:29 -0700

    Load market intelligence before UI initialization

--- app-ui.js
+++ app-ui.js
@@ -1,1 +1,1 @@
-document.write('<script src="app-ui-a.js"><\/script><script src="app-ui-b.js"><\/script><script src="app-ui-c.js"><\/script>');
+document.write('<script src="app-market.js"><\/script><script src="app-ui-a.js"><\/script><script src="app-ui-b.js"><\/script><script src="app-ui-c.js"><\/script>');
