2d68674ea6cebff613d36c79388816f944dfc045
Michael Stock <mistock@outlook.com>
2026-08-14 10:50:37 -0700

    Load market intelligence styling

--- app-ui.js
+++ app-ui.js
@@ -1,1 +1,5 @@
+const marketStyle = document.createElement('link');
+marketStyle.rel = 'stylesheet';
+marketStyle.href = 'market.css';
+document.head.appendChild(marketStyle);
 document.write('<script src="app-market.js"><\/script><script src="app-ui-a.js"><\/script><script src="app-ui-b.js"><\/script><script src="app-ui-c.js"><\/script>');
