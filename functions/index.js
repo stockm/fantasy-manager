adf7dd50a06f416f346d204599113b015b6ba789
Michael Stock <mistock@outlook.com>
2026-08-16 03:34:52 -0700

    Export scheduled and manual cache refresh functions

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,5 @@ setGlobalOptions({ region: 'us-central1', maxInstances: 10 });
 exports.aiAdvice = require('./ai/advice').aiAdvice;
 exports.nflWeek = require('./nfl/week').nflWeek;
 exports.screenshotImport = require('./screenshot-import').screenshotImport;
+exports.precomputeFantasyCaches = require('./cache/derived').precomputeFantasyCaches;
+exports.refreshDerivedCache = require('./cache/derived').refreshDerivedCache;
